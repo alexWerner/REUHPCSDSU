93105d8e4532f334a275e1617d3d297e91e9183d
alexWerner <alexwerner@wi.rr.com>
2018-06-12 10:33:06 -0500

    Got an IS example working properly

--- proj/ISTest/isTest2.c
+++ proj/ISTest/isTest2.c
@@ -25,6 +25,8 @@ int main(int argc,char **argv)
   VecAssemblyBegin(v);CHKERRQ(ierr);
   VecAssemblyEnd(v);CHKERRQ(ierr);
 
+  ierr = VecView(v, PETSC_VIEWER_STDOUT_WORLD);
+
   ierr = VecGetArrayRead(v, &vArr);CHKERRQ(ierr);
   PetscInt min, max, n;
   ierr = VecGetOwnershipRange(v, &min, &max);CHKERRQ(ierr);
@@ -33,25 +35,38 @@ int main(int argc,char **argv)
   for(PetscInt i = min; i < max; i++)
   {
     if(vArr[i - min] == 0)
-      vals[i - min] = -1;
+      vals[i - min] = max;
     else
       vals[i - min] = i;
   }
 
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "Vals: %D", vals);CHKERRQ(ierr);
+
   ierr = ISCreateGeneral(PETSC_COMM_WORLD, n, vals, PETSC_COPY_VALUES, &is);CHKERRQ(ierr);
+
+  IS isNeg, isOut;
+
+  ierr = ISCreateGeneral(PETSC_COMM_WORLD, 1, &max, PETSC_COPY_VALUES, &isNeg);CHKERRQ(ierr);
 
   ierr = VecRestoreArrayRead(v, &vArr);CHKERRQ(ierr);
 
   ierr = ISView(is, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  ierr = ISView(isNeg, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+
+  ierr = ISDifference(is, isNeg, &isOut);CHKERRQ(ierr);
+
+  ierr = ISView(isOut, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
   Vec sub;
-  ierr = VecGetSubVector(v, is, &sub);CHKERRQ(ierr);
+  ierr = VecGetSubVector(v, isOut, &sub);CHKERRQ(ierr);
 
   ierr = VecView(sub, PETSC_VIEWER_STDOUT_WORLD);
 
   ierr = VecRestoreSubVector(v, is, &sub);CHKERRQ(ierr);
 
   ierr = ISDestroy(&is);CHKERRQ(ierr);
+  ierr = ISDestroy(&isNeg);CHKERRQ(ierr);
+  ierr = ISDestroy(&isOut);CHKERRQ(ierr);
   ierr = PetscFinalize();CHKERRQ(ierr);
   return ierr;
 }
